dd97099fe2e07cf526833a5ad983e0bc8a72fb07
sjohn248 <89377875+sjohn248@users.noreply.github.com>
2022-10-20 01:32:24 -0400

    Create generateKey.sh

---
+++ V2/generateCerts/generateKey.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+openssl genrsa -aes256 -out priv.pem 4096
+cat priv.pem
+openssl rsa -text -in priv.pem
+openssl rsa -in priv.pem -pubout -out pub.pem
+
+openssl req -new -key priv.pem -out cert.csr
+openssl req -text -in cert.csr -noout
+openssl x509 -req -days 365 -in cert.csr -signkey priv.pem -out cert.crt
